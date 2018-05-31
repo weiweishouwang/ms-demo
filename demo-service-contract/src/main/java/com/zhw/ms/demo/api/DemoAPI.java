52d41c21f3fdc7ab9170a86d9bf144567d4646c7
weiweishouwang <weiweishouwang@126.com>
2018-05-31 11:11:32 +0800

    1

---
+++ demo-service-contract/src/main/java/com/zhw/ms/demo/api/DemoAPI.java
@@ -0,0 +1,16 @@
+package com.zhw.ms.demo.api;
+
+import com.zhw.ms.commons.bean.Result;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+public interface DemoAPI {
+
+    @RequestMapping(value = "/getAdmin", method = RequestMethod.GET)
+    public Result<Object> getAdmin(@RequestParam("id") Long id);
+
+    @RequestMapping(value = "/throwException", method = RequestMethod.GET)
+    public Result<Object> throwException() throws Exception;
+
+}

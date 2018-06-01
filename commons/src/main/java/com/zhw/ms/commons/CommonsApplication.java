82ef28d0100e4981cd6344e5bc65d31adc0c5d6e
weiweishouwang <weiweishouwang@126.com>
2018-06-01 11:20:56 +0800

    1

--- commons/src/main/java/com/zhw/ms/commons/CommonsApplication.java
+++ commons/src/main/java/com/zhw/ms/commons/CommonsApplication.java
@@ -3,11 +3,15 @@ package com.zhw.ms.commons;
 import com.zhw.ms.commons.bean.Result;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @SpringBootApplication
+@ServletComponentScan
+@EnableAsync
 public class CommonsApplication {
     public static void main(String[] args) {
         SpringApplication.run(CommonsApplication.class, args);
